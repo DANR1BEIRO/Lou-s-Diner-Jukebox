4b6510dba771af0eb1157b37b145eca15bab96f8
DANR1BEIRO <danribeirodev@gmail.com>
2025-11-23 12:44:20 -0300

    feat: Compara por artista utilizando classe interna anônima definida por argumento

--- src/LousDinerJukebox/Jukebox1.java
+++ src/LousDinerJukebox/Jukebox1.java
@@ -1,6 +1,7 @@
 package LousDinerJukebox;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Jukebox1 {
@@ -15,11 +16,23 @@ public class Jukebox1 {
     public void go() {
         List<Song> songList = MockSongs.getSongsObjects();
         System.out.println("Lista em ordem de inserção: " + songList);
+        // Usa Comparable para ordenar
         Collections.sort(songList);
         System.out.println("Lista ordenada naturalmente pelo sort: " + songList);
 
         ArtistCompare artistCompare = new ArtistCompare();
+        // Usa uma classe Comparator customizada para ordenar
         songList.sort(artistCompare);
         System.out.println("Comparando por artista utilizando um compare: " + songList);
+
+        songList.sort(new Comparator<Song>() {
+            @Override
+            public int compare(Song song, Song t1) {
+                return song.getArtist().compareTo(t1.getArtist());
+            }
+        });
+        System.out.println("Ordena a lista pelo nome do artista usando um Comparator " +
+                "implementado como classe interna anônima passada diretamente no método sort: \n "
+                + songList);
     }
 }
