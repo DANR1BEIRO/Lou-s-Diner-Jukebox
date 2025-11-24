edcafe176a89a5a5b41b5ed775f9ca759f7b81da
DANR1BEIRO <danribeirodev@gmail.com>
2025-11-24 12:33:40 -0300

    feat: Cria a classe MockMoreSongs com valores duplicados para testar a implementacão de Set

--- src/LousDinerJukebox/MockMoreSongs.java
+++ src/LousDinerJukebox/MockMoreSongs.java
@@ -1,4 +1,19 @@
 package LousDinerJukebox;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MockMoreSongs {
+    public static List<Song> getSongsObjects() {
+        List<Song> songs = new ArrayList<>(List.of(
+                new Song("somersault", "zero 7", 147),
+                new Song("cassidy", "grateful dead", 158),
+                new Song("$10", "hitchhiker", 140),
+                new Song("havana", "cabello", 105),
+                new Song("$10", "hitchhiker", 140),
+                new Song("cassidy", "grateful dead", 158),
+                new Song("50 ways", "simon", 102)));
+
+        return songs;
+    }
 }
