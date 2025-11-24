189798eb20058fb82bb3b6536e0e488daeaf16fe
DANR1BEIRO <danribeirodev@gmail.com>
2025-11-24 12:35:05 -0300

    test: Testa hashCode e equals

--- src/LousDinerJukebox/HashCodeAndEqualsTest01.java
+++ src/LousDinerJukebox/HashCodeAndEqualsTest01.java
@@ -1,4 +1,22 @@
 package LousDinerJukebox;
 
 public class HashCodeAndEqualsTest01 {
+    public static void main(String[] args) {
+        String name1 = new String("goku");
+        String name2 = name1;
+        System.out.println(System.identityHashCode(name1));
+        System.out.println(System.identityHashCode(name2));
+        System.out.println("Tem o mesmo conteúdo (valor): " + name1.equals(name2));
+        System.out.print("Referenciam o mesmo objeto na memória: ");
+        System.out.println(true ? name1 == name2 : false);
+
+        String name3 = new String("goku");
+        String name4 = "goku";
+        System.out.println(System.identityHashCode(name3));
+        System.out.println(System.identityHashCode(name4));
+        System.out.println("Tem o mesmo conteúdo (valor): " + name3.equals(name4));
+        System.out.print("Referenciam o mesmo objeto na memória: ");
+        System.out.println(true ? name3 == name4 : false);
+    }
 }
+
