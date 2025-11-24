47cec39c0e74e16c47bfb898b39c558532987365
DANR1BEIRO <danribeirodev@gmail.com>
2025-11-24 12:34:29 -0300

    feat: Cria a classe Jukebox4 para testar a implementacão de hashSet

--- src/LousDinerJukebox/Jukebox4.java
+++ src/LousDinerJukebox/Jukebox4.java
@@ -1,4 +1,28 @@
 package LousDinerJukebox;
 
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Jukebox4 {
+    public static void main(String[] args) {
+        Jukebox4.go();
+    }
+
+    public static void go() {
+        List<Song> songList = MockMoreSongs.getSongsObjects();
+        System.out.println(songList);
+
+        songList.sort((s1, s2) -> s1.getTitle().compareTo(s2.getTitle()));
+        System.out.println(songList);
+
+        Set<Song> songSet = new HashSet<>(songList);
+        System.out.println(songSet);
+
+        System.out.println(System.identityHashCode(songList));
+        System.out.println(System.identityHashCode(songSet));
+        System.out.println(true ? songList.equals(songSet) : false);
+    }
 }
