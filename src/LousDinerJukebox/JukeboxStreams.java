1d4887a74dabe8c2893f355da9f32f6fcbadee45
DANR1BEIRO <danribeirodev@gmail.com>
2025-12-30 13:18:44 -0300

    feat: Cria a JukeboxStreams para aplicar os filtros utilizando Streams

--- src/LousDinerJukebox/JukeboxStreams.java
+++ src/LousDinerJukebox/JukeboxStreams.java
@@ -1,4 +1,15 @@
 package LousDinerJukebox;
 
+import java.util.List;
+
 public class JukeboxStreams {
+    public static void main(String[] args) {
+        Songs songs = new Songs();
+
+        List<Song> rock = songs.getSongs().stream()
+                .filter(song -> song.getGenre().equalsIgnoreCase("rock"))
+                .toList();
+
+        System.out.println(rock);
+    }
 }
